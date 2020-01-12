caa3ff107aacc843df672d2f584b79186d65f2a4
easternDay <849919718@qq.com>
2020-01-12 13:33:06 +0800

    初始项目

---
+++ components/BookCard.vue
@@ -0,0 +1,20 @@
+<template name="BookCard">
+	<view>
+		<text>这是一段测试文字</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"BookCard",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
